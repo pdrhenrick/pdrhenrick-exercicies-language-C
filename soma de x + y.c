a90ede246792eb11736bb79bad7f8096d19d5575
pedro <63935909+pdrhenrick@users.noreply.github.com>
2024-03-28 20:34:08 -0300

    Add files via upload

---
+++ soma de x + y.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+  int x = 10;
+  int y = 20;
+  //int soma = x + y;
+  
+  printf("A soma é: %i", x + y);
+  
+    return 0;
+}
+
