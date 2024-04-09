0ee409d7a02917c0c37a2821881e6b31516ca0ec
pedro <63935909+pdrhenrick@users.noreply.github.com>
2024-04-09 18:38:05 -0300

    Add files via upload

---
+++ tabuada.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    
+    printf("Digite o numero: ");
+    scanf("%i", &numero);
+    
+    printf("%i x 1 =%i\n ", numero, numero * 1);
+    printf("%i x 2 =%i\n ", numero, numero * 2);
+    printf("%i x 3 =%i\n ", numero, numero * 3);
+    printf("%i x 4 =%i\n ", numero, numero * 4);
+    printf("%i x 5 =%i\n ", numero, numero * 5);
+    printf("%i x 6 =%i\n ", numero, numero * 6);
+    printf("%i x 7 =%i\n ", numero, numero * 7);
+    printf("%i x 8 =%i\n ", numero, numero * 8);
+    printf("%i x 9 =%i\n ", numero, numero * 9);
+    printf("%i x 10 =%i\n ", numero, numero * 10);
+    printf("%i x 11 =%i\n ", numero, numero * 11);
+    printf("%i x 12 =%i\n ", numero, numero * 12);
+    printf("%i x 13 =%i\n ", numero, numero * 13);
+    printf("%i x 14 =%i\n ", numero, numero * 14);
+    printf("%i x 15 =%i\n ", numero, numero * 15);
+                  
+    
+    
+    return 0;
+}
+
