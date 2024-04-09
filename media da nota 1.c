af8f38d7754f2cd925026b4cceb83c36cdd809ca
pedro <63935909+pdrhenrick@users.noreply.github.com>
2024-04-09 18:22:03 -0300

    Add files via upload

---
+++ media da nota 1.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    
+    int nota1 = 8.6;
+    int nota2 = 4.5;
+    //int soma = x + y;
+    
+    printf("a nota é: %i", nota1 + nota2);
+    
+    int nota = 12;
+    int divisor = 2;
+    float resultado;
+    
+    resultado = (float) nota / divisor;
+    
+    printf(" A média da nota e: %.2f\n", resultado);
+
+    return 0;
+}
+
+
