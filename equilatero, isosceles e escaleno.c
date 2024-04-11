95d10aac9d742de1744790a115d3425c97989ed4
pedro <63935909+pdrhenrick@users.noreply.github.com>
2024-04-11 20:15:19 -0300

    Add files via upload

---
+++ equilatero, isosceles e escaleno.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    float lado1, lado2, lado3;
+
+    printf("Digite os três lados do triângulo separados por espaços: ");
+    scanf("%f %f %f", &lado1, &lado2, &lado3);
+
+    if (lado1 == lado2 && lado2 == lado3) {
+        printf("O triângulo é equilátero.\n");
+    } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+        printf("O triângulo é isósceles.\n");
+    } else {
+        printf("O triângulo é escaleno.\n");
+    }
+
+    return 0;
+}
+
